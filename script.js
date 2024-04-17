9e3fc46db8340309f1b2bdc13e144e0cb9d37f2d
Bernardo Fernandez <118931906+bernardofernandezz@users.noreply.github.com>
2024-04-17 08:41:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function toggleMode() {
     html.classList.toggle("light")
   
     // pegar a tag img
-    const img = document.querySelector("#profile img")
+    const img = document.querySelector ("#profile img")
+
   
     // substituir a imagem
     if (html.classList.contains("light")) {
